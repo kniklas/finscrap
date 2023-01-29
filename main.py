81302986f98c037b99d393c2c882225f2abfd1a4
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2023-01-29 21:54:30 +0000

    Add TODO and remove old comments

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import bs4
 
+# TODO
+# - add another page interface
+# - add logging errors/info
+
 isin_urls = {
     "analizy.pl": {
         "ISIN1": "https://www.analizy.pl/fundusze-inwestycyjne-otwarte/ALL90/allianz-dochodowy-income-and-growth",
@@ -10,14 +14,9 @@ isin_urls = {
 
 sel = isin_urls['analizy.pl']
 for isin in sel.keys():
-    #  print(sel[isin])
     resp = requests.get(sel[isin])
     soup = bs4.BeautifulSoup(resp.content, "html.parser")
     date = soup.find("p", class_="lightProductText").text
     price = soup.find("span", class_="productBigText").text
 
     print("ISIN:", isin, "\tDate:", date, "\tPrice:", price)
-
-#  s2 = soup.find_all("span", class_="productBigText")
-# https://realpython.com/beautiful-soup-web-scraper-python/
-# https://realpython.com/beautiful-soup-web-scraper-python/#find-elements-by-html-class-name
