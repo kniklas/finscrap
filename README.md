ba34bd8a1c6eb43c13445c6961d45293cfdf592a
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2023-02-03 22:21:09 +0000

    Refactor to Object Oriented and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# fin-webscrap
-Financial web scraping
+# fin-webscrap (WIP)
+
+Financial web scraping - Work In Progress
+
+Objective of this project is to webscrap financial data and provide API
+deployed in the cloud to access the data.
--- main.py
+++ main.py
@@ -3,44 +3,79 @@ import bs4
 import funds as funds
 
 # TODO
-# - check if yahoo works with xlml
-# - add another page interface
 # - add logging errors/info, see exception handling in Web Scraping with Python
 # - add pytest with mocks to requests.get, bs4.BeatutifulSoup, soup.find
 # - add method to retrieve NBP currency rates
 # - calculate requirements for DB size
 
 
-#  sel = funds.funds_urls['analizy.pl']
-#  for isin in sel.keys():
-#      resp = requests.get(sel[isin])
-#      soup = bs4.BeautifulSoup(resp.content, "html.parser")
-#      date = soup.find("p", class_="lightProductText").text
-#      price = soup.find("span", class_="productBigText").text.replace(",", ".")
-#      #  print("ISIN:", isin, "\tDate:", date, "\tPrice:", price)
-#      print(f"{isin},{date},{price}")
+class GetAsset():
+    def __init__(self, site):
+        self.sel = funds.funds_urls[site]
+        print(f"Initialize: {site}")
 
-#  sel = funds.funds_urls['biznesradar.pl']
-#  for isin in sel.keys():
-#      resp = requests.get(sel[isin])
-#      soup = bs4.BeautifulSoup(resp.content, "html.parser")
-#      date = soup.find("time", class_="q_ch_date")["datetime"][0:10]
-#      price = soup.find("span", class_="q_ch_act").text
-#      #  print("ISIN:", isin, "\tDate:", date, "\tPrice:", price)
-#      print(f"{isin},{date},{price}")
+    def get_date(self, soup):
+        pass
 
-sel = funds.funds_urls['borsa']
-for isin in sel.keys():
-    resp = requests.get(sel[isin])
-    soup = bs4.BeautifulSoup(resp.content, "lxml")
-    date = soup.find("span", class_="t-text -block -size-xs | -xs").strong.text[0:8]
-    price = soup.find("span", class_="t-text -black-warm-60 -formatPrice").strong.text
-    print(f"{isin},{date},{price}")
+    def get_price(self, soup):
+        pass
 
-sel = funds.funds_urls['ishares']
-for isin in sel.keys():
-    resp = requests.get(sel[isin])
-    soup = bs4.BeautifulSoup(resp.content, "lxml")
-    date = soup.find("span", class_="header-nav-label navAmount").text[11:23].replace(" ", "/").replace(",", "")
-    price = soup.find("span", class_="header-nav-data").text[2:].strip()
-    print(f"{isin},{date},{price}")
+    def get_data(self):
+        d = dict()
+        for isin in self.sel.keys():
+            resp = requests.get(self.sel[isin])
+            soup = bs4.BeautifulSoup(resp.content, "lxml")
+            date = self.get_date(soup)
+            price = self.get_price(soup)
+            print(f"{isin},{date},{price}")
+            d[isin] = (date, price)
+        return d
+
+
+class GetAssetAnalizy(GetAsset):
+    def get_date(self, soup):
+        return soup.find("p", class_="lightProductText").text
+
+    def get_price(self, soup):
+        return soup.find("span",
+                         class_="productBigText").text.replace(",", ".")
+
+
+class GetAssetBiznesR(GetAsset):
+    def get_date(self, soup):
+        return soup.find("time", class_="q_ch_date")["datetime"][0:10]
+
+    def get_price(self, soup):
+        return soup.find("span", class_="q_ch_act").text
+
+
+class GetAssetBorsa(GetAsset):
+    def get_date(self, soup):
+        d = soup.find("span", class_="t-text -block -size-xs | -xs")
+        return d.strong.text[0:8]
+
+    def get_price(self, soup):
+        p = soup.find("span", class_="t-text -black-warm-60 -formatPrice")
+        return p.strong.text
+
+
+class GetAssetIShares(GetAsset):
+    def get_date(self, soup):
+        d = soup.find("span", class_="header-nav-label navAmount")
+        return d.text[11:23].replace(" ", "/").replace(",", "")
+
+    def get_price(self, soup):
+        return soup.find("span", class_="header-nav-data").text[2:].strip()
+
+
+analizy = GetAssetAnalizy('analizy.pl')
+print(analizy.get_data())
+
+biznesr = GetAssetBiznesR('biznesradar.pl')
+print(biznesr.get_data())
+
+borsa = GetAssetBorsa('borsa')
+print(borsa.get_data())
+
+ishares = GetAssetIShares('ishares')
+print(ishares.get_data())
