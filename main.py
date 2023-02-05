2340bc8773f8a4636115628cfd42c45de6b09642
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2023-02-05 02:28:19 +0000

    Add error handling and apply DRY

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import requests
 import bs4
 import funds as funds
+from urllib import request
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 
 # TODO
-# - add logging errors/info, see exception handling in Web Scraping with Python
-# - add pytest with mocks to requests.get, bs4.BeatutifulSoup, soup.find
+# - add pytest with mocks to requests, test parsing/formatting data, dict, etc.
+# - make sure date is stored/parsed in same format
+# - add GitHub actions, pre-commit
+# - put logic in python module (public) with documented support of fin webs
+# - add logging (as option for the package?)
+
+# TODO (later)
 # - add method to retrieve NBP currency rates
 # - calculate requirements for DB size
 
@@ -14,68 +23,120 @@ class GetAsset():
         self.sel = funds.funds_urls[site]
         print(f"Initialize: {site}")
 
-    def get_date(self, soup):
-        pass
-
-    def get_price(self, soup):
-        pass
+    def get_element(self, soup, tag, class_id):
+        d = soup.find(tag, class_id)
+        if d is None:
+            raise AttributeError(f'Incorrect <{tag}> or class=\'{class_id}\'')
+        return d
 
     def get_data(self):
         d = dict()
         for isin in self.sel.keys():
-            resp = requests.get(self.sel[isin])
-            soup = bs4.BeautifulSoup(resp.content, "lxml")
-            date = self.get_date(soup)
-            price = self.get_price(soup)
-            print(f"{isin},{date},{price}")
-            d[isin] = (date, price)
+            try:
+                req = request.Request(self.sel[isin])
+                urlopen(self.sel[isin])
+            except HTTPError as e:
+                print(f'{e} | Verify if URL is correct: {req.full_url}')
+                date, price = None, None
+            except URLError as e:
+                print(f'{e} | Verify if domain name is correct: {req.host}')
+                date, price = None, None
+            else:
+                resp = requests.get(self.sel[isin])
+                soup = bs4.BeautifulSoup(resp.content, "lxml")
+                try:
+                    date = self.get_date(soup)
+                except (AttributeError, TypeError) as e:
+                    print(f'Cannot extract date from {req.full_url} - {e}')
+                    date = None
+
+                try:
+                    price = self.get_price(soup)
+                except (AttributeError, TypeError) as e:
+                    print(f'Cannot extract price from: {req.full_url} - {e} -')
+                    price = None
+
+                print(f"{isin},{date},{price}")
+                d[isin] = (date, price)
         return d
 
 
 class GetAssetAnalizy(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "p"
+        self.date_class = "lightProductText"
+        self.price_tag = "span"
+        self.price_class = "productBigText"
+
     def get_date(self, soup):
-        return soup.find("p", class_="lightProductText").text
+        d = super().get_element(soup, self.date_tag, self.date_class)
+        return d.text
 
     def get_price(self, soup):
-        return soup.find("span",
-                         class_="productBigText").text.replace(",", ".")
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.text.replace(",", ".")
 
 
 class GetAssetBiznesR(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "time"
+        self.date_class = "q_ch_date"
+        self.price_tag = "span"
+        self.price_class = "q_ch_act"
+
     def get_date(self, soup):
-        return soup.find("time", class_="q_ch_date")["datetime"][0:10]
+        d = super().get_element(soup, self.date_tag, self.date_class)
+        return d["datetime"][0:10]
 
     def get_price(self, soup):
-        return soup.find("span", class_="q_ch_act").text
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.text
 
 
 class GetAssetBorsa(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "span"
+        self.date_class = "t-text -block -size-xs | -xs"
+        self.price_tag = "span"
+        self.price_class = "t-text -black-warm-60 -formatPrice"
+
     def get_date(self, soup):
-        d = soup.find("span", class_="t-text -block -size-xs | -xs")
+        d = super().get_element(soup, self.date_tag, self.date_class)
         return d.strong.text[0:8]
 
     def get_price(self, soup):
-        p = soup.find("span", class_="t-text -black-warm-60 -formatPrice")
+        p = super().get_element(soup, self.price_tag, self.price_class)
         return p.strong.text
 
 
 class GetAssetIShares(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "span"
+        self.date_class = "header-nav-label navAmount"
+        self.price_tag = "span"
+        self.price_class = "header-nav-data"
+
     def get_date(self, soup):
-        d = soup.find("span", class_="header-nav-label navAmount")
+        d = super().get_element(soup, self.date_tag, self.date_class)
         return d.text[11:23].replace(" ", "/").replace(",", "")
 
     def get_price(self, soup):
-        return soup.find("span", class_="header-nav-data").text[2:].strip()
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.text[2:].strip()
 
 
 analizy = GetAssetAnalizy('analizy.pl')
-print(analizy.get_data())
+analizy.get_data()
 
 biznesr = GetAssetBiznesR('biznesradar.pl')
-print(biznesr.get_data())
+biznesr.get_data()
 
 borsa = GetAssetBorsa('borsa')
-print(borsa.get_data())
+borsa.get_data()
 
 ishares = GetAssetIShares('ishares')
-print(ishares.get_data())
+ishares.get_data()
