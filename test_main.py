534cf05066799d9c5c926dd070dec1d67ba45b1f
Kamil Niklasinski <kamil.niklasinski@gmail.com>
2023-02-10 22:37:36 +0000

    Error handling and pytest (#1)
    * Error / exception handling
    * Pytest unit tests with mocks for network / request calls
    * Clean-up of code and comments

---
+++ test_main.py
@@ -0,0 +1,164 @@
+import pytest
+import requests
+import bs4
+import urllib
+from urllib import request
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+
+import webscrap
+
+
+ANALIZY_PL = """
+<p class=\'lightProductText\'>12.2</p>
+<span class=\'productBigText\'>2022-12-01</span>
+"""
+
+ANALIZY_PL_bad_price_class = """
+<p class=\'lightProductText2\'>12.2</p>
+<span class=\'productBigText\'>2022-12-01</span>
+"""
+
+ANALIZY_PL_bad_date_class = """
+<p class=\'lightProductText\'>12.2</p>
+<span class=\'productBigText2\'>2022-12-01</span>
+"""
+
+ANALIZY_PL_bad_price_tag = """
+<l class=\'lightProductText\'>12.2</l>
+<span class=\'productBigText2\'>2022-12-01</span>
+"""
+
+ANALIZY_PL_bad_date_tag = """
+<p class=\'lightProductText\'>12.2</p>
+<div class=\'productBigText2\'>2022-12-01</div>
+"""
+
+
+class DummySoup(bs4.BeautifulSoup):
+    def __init__(self, in_soup):
+        super().__init__(in_soup, "lxml")
+
+
+class DummyRequestGet:
+    def __init__(self):
+        self.content = "Request Get Dummy content"
+
+
+@pytest.fixture(name="analizy_web")
+def fixture_analizy_web():
+    analizy_web = webscrap.GetAssetAnalizy("analizy.pl")
+    yield analizy_web
+
+
+@pytest.fixture(name="borsa_web")
+def fixture_borsa_web():
+    borsa_web = webscrap.GetAssetBorsa("borsa")
+    yield borsa_web
+
+
+@pytest.fixture(name="ishares_web")
+def fixture_ishares_web():
+    ishares_web = webscrap.GetAssetIShares("ishares")
+    yield ishares_web
+
+
+def test_borsa_date_conversion(borsa_web):
+    input_date = "23/02/10"
+    assert borsa_web.convert_date(input_date) == "2023-02-10"
+
+
+def test_analizy_date_conversion(analizy_web):
+    input_date = "09.02.2023"
+    assert analizy_web.convert_date(input_date) == "2023-02-09"
+
+
+def test_ishares_date_conversion(ishares_web):
+    input_date = "Feb/09/2023"
+    assert ishares_web.convert_date(input_date) == "2023-02-09"
+
+
+def test_analizy_pl_initialisation(analizy_web):
+    assert analizy_web
+
+
+def test_ishares_initialisation(ishares_web):
+    assert ishares_web
+
+
+def test_biznesradar_pl_initialisation():
+    biznesradar_web = webscrap.GetAssetAnalizy("biznesradar.pl")
+    assert biznesradar_web
+
+
+def test_concatenate_date(analizy_web):
+    assert analizy_web.concatenate_date("01", "02", "2022") == "2022-02-01"
+
+
+def test_get_data_raise_URLError(mocker, analizy_web):
+    mocker.patch.object(
+        urllib.request,
+        "urlopen",
+        side_effect=URLError("BAD URL"),
+    )
+    result = analizy_web.get_data()
+    assert result == {"I01": (None, None)}
+
+
+def test_get_data_raise_HTTPError(mocker, analizy_web):
+    mocker.patch.object(
+        urllib.request,
+        "urlopen",
+        side_effect=HTTPError("OJ!", 404, "aa", None, None),
+    )
+    result = analizy_web.get_data()
+    assert result == {"I01": (None, None)}
+
+
+@pytest.mark.parametrize(
+    "html, tag, id_",
+    (
+        (ANALIZY_PL_bad_price_class, "p", "lightProductText"),
+        (ANALIZY_PL_bad_price_tag, "p", "lightProductText"),
+        (ANALIZY_PL_bad_date_class, "span", "productBigText"),
+        (ANALIZY_PL_bad_date_tag, "span", "productBigText"),
+    ),
+)
+def test_analizy_get_element_exception(analizy_web, html, tag, id_):
+    with pytest.raises(AttributeError):
+        soup = bs4.BeautifulSoup(html, "lxml")
+        analizy_web.get_element(soup, tag, id_)
+
+
+@pytest.mark.parametrize(
+    "html, expected_result",
+    (
+        (ANALIZY_PL, {"I01": ("12.2", "2022-12-01")}),
+        (ANALIZY_PL_bad_price_class, {"I01": (None, "2022-12-01")}),
+        (ANALIZY_PL_bad_date_class, {"I01": ("12.2", None)}),
+    ),
+)
+def test_analizy_get_data(mocker, analizy_web, html, expected_result):
+    mocker.patch.object(request, "urlopen", return_value="urlopen web res")
+    mocker.patch.object(requests, "get", return_value=DummyRequestGet())
+    mocker.patch.object(bs4, "BeautifulSoup", return_value=DummySoup(html))
+    result = analizy_web.get_data()
+    assert result == expected_result
+
+
+# TODO: consider if this is not duplicate of test_get_data()
+@pytest.mark.parametrize(
+    "tag, id_, expected_result",
+    (
+        ("p", "lightProductText", '<p class="lightProductText">12.2</p>'),
+        (
+            "span",
+            "productBigText",
+            '<span class="productBigText">2022-12-01</span>',
+        ),
+    ),
+)
+def test_analizy_get_element_found(analizy_web, tag, id_, expected_result):
+    soup = bs4.BeautifulSoup(ANALIZY_PL, "lxml")
+    assert str(analizy_web.get_element(soup, tag, id_)) == expected_result
---
+++ webscrap.py
@@ -0,0 +1,157 @@
+import requests
+import bs4
+import funds as funds
+from urllib import request
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+
+class GetAsset:
+    def __init__(self, site):
+        self.sel = funds.funds_urls[site]
+        print(f"Initialize: {site}")
+
+    @staticmethod
+    def concatenate_date(day, month, year):
+        return f"{year}-{month}-{day}"
+
+    def get_element(self, soup, tag, class_id):
+        d = soup.find(tag, class_id)
+        if d is None:
+            raise AttributeError(f"Incorrect <{tag}> or class='{class_id}'")
+        return d
+
+    def get_data(self):
+        d = dict()
+        for isin in self.sel.keys():
+            try:
+                # Required to get URL or hostname in error handling
+                req = request.Request(self.sel[isin])
+                # Catching actual exception
+                request.urlopen(self.sel[isin])
+            except HTTPError as e:
+                print(f"{e} | Verify if URL is correct: {req.full_url}")
+                date, price = None, None
+            except URLError as e:
+                print(f"{e} | Verify if domain name is correct: {req.host}")
+                date, price = None, None
+            else:
+                resp = requests.get(self.sel[isin])
+                soup = bs4.BeautifulSoup(resp.content, "lxml")
+                try:
+                    date = self.get_date(soup)
+                except (AttributeError, TypeError) as e:
+                    print(f"Cannot extract date from {req.full_url} - {e}")
+                    date = None
+                try:
+                    price = self.get_price(soup)
+                except (AttributeError, TypeError) as e:
+                    print(f"Cannot extract price from: {req.full_url} - {e}")
+                    price = None
+                print(f"{isin},{date},{price}")
+            d[isin] = (date, price)
+        return d
+
+
+class GetAssetAnalizy(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "p"
+        self.date_class = "lightProductText"
+        self.price_tag = "span"
+        self.price_class = "productBigText"
+
+    def get_date(self, soup):
+        d = super().get_element(soup, self.date_tag, self.date_class)
+        return d.text
+
+    def get_price(self, soup):
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.text.replace(",", ".")
+
+    @staticmethod
+    def convert_date(input_date):
+        day = input_date[0:2]
+        month = input_date[3:5]
+        year = input_date[6:10]
+        return GetAsset.concatenate_date(day, month, year)
+
+
+class GetAssetBiznesR(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "time"
+        self.date_class = "q_ch_date"
+        self.price_tag = "span"
+        self.price_class = "q_ch_act"
+
+    def get_date(self, soup):
+        d = super().get_element(soup, self.date_tag, self.date_class)
+        return d["datetime"][0:10]
+
+    def get_price(self, soup):
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.text
+
+
+class GetAssetBorsa(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "span"
+        self.date_class = "t-text -block -size-xs | -xs"
+        self.price_tag = "span"
+        self.price_class = "t-text -black-warm-60 -formatPrice"
+
+    def get_date(self, soup):
+        d = super().get_element(soup, self.date_tag, self.date_class)
+        return d.strong.text[0:8]
+
+    def get_price(self, soup):
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.strong.text
+
+    @staticmethod
+    def convert_date(input_date):
+        day = input_date[6:8]
+        month = input_date[3:5]
+        year = "20" + input_date[0:2]
+        print(year, month, day)
+        return GetAsset.concatenate_date(day, month, year)
+
+
+class GetAssetIShares(GetAsset):
+    def __init__(self, site):
+        super().__init__(site)
+        self.date_tag = "span"
+        self.date_class = "header-nav-label navAmount"
+        self.price_tag = "span"
+        self.price_class = "header-nav-data"
+
+    def get_date(self, soup):
+        d = super().get_element(soup, self.date_tag, self.date_class)
+        return d.text[11:23].replace(" ", "/").replace(",", "")
+
+    def get_price(self, soup):
+        p = super().get_element(soup, self.price_tag, self.price_class)
+        return p.text[2:].strip()
+
+    @staticmethod
+    def convert_date(input_date):
+        month_dict = {
+            "Jan": "01",
+            "Feb": "02",
+            "Mar": "03",
+            "Apr": "04",
+            "May": "05",
+            "Jun": "06",
+            "Jul": "07",
+            "Aug": "08",
+            "Sep": "09",
+            "Oct": "10",
+            "Nov": "11",
+            "Dec": "12",
+        }
+        day = input_date[4:6]
+        month = month_dict[input_date[0:3]]
+        year = input_date[7:11]
+        return GetAsset.concatenate_date(day, month, year)
