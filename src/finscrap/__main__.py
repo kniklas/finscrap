84408f7a47722068f241db2ea6b95b61e046b1ec
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2023-03-07 22:35:42 +0000

    Add funds config from json file
    It replaces functionality which assumed to load funds configuration as
    funds.py file.

--- src/finscrap/__main__.py
+++ src/finscrap/__main__.py
@@ -1,18 +1,31 @@
 """Loader and executor of finscrap module."""
 
-if __name__ == "__main__":
-    from finscrap import finscrap
+import json
+import click
 
-    print("LOADING")
+from finscrap import finscrap
 
-    analizy = finscrap.GetAssetAnalizy("analizy.pl")
+
+@click.command()
+@click.argument("filename")
+def read_config(filename):
+    "Load funds config as dictionary"
+    with open(filename, "r", encoding="utf-8") as fund_config:
+        funds_urls = json.load(fund_config)
+
+    analizy = finscrap.GetAssetAnalizy("analizy.pl", funds_urls)
     analizy.get_data()
 
-    biznesr = finscrap.GetAssetBiznesR("biznesradar.pl")
+    biznesr = finscrap.GetAssetBiznesR("biznesradar.pl", funds_urls)
     biznesr.get_data()
 
-    borsa = finscrap.GetAssetBorsa("borsa")
+    borsa = finscrap.GetAssetBorsa("borsa", funds_urls)
     borsa.get_data()
 
-    ishares = finscrap.GetAssetIShares("ishares")
+    ishares = finscrap.GetAssetIShares("ishares", funds_urls)
     ishares.get_data()
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    read_config()
