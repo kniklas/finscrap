0e22884a76ec1b04b5865124f6a470f47010492b
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2023-03-08 22:12:33 +0000

    Change arguments to use path instead of file
    - changed name from 'filename' to 'funds_json', along with type: not file,
    but path
    - added '--csv' option, however not yet implemented as it requires
      significant rework, how data will be saved to csv. Ideally a handler
      function or class needs to be created, which deals with multiple funds
      configurations, ignore not created configs if required.

--- src/finscrap/__main__.py
+++ src/finscrap/__main__.py
@@ -7,23 +7,27 @@ from finscrap import finscrap
 
 
 @click.command()
-@click.argument("filename")
-def read_config(filename):
-    "Load funds config as dictionary"
-    with open(filename, "r", encoding="utf-8") as fund_config:
+@click.argument("funds_json", type=click.Path(exists=True))
+@click.option("-c", "--csv", type=click.Path(), help="Path to CSV output file")
+def read_config(funds_json, csv):
+    """Reads funds prices and valuation dates from defined URLs via FUNDS_JSON
+    definition file.
+    """
+    with open(funds_json, "r", encoding="utf-8") as fund_config:
         funds_urls = json.load(fund_config)
 
+    print("OUTPUT_CSV", csv)
     analizy = finscrap.GetAssetAnalizy("analizy.pl", funds_urls)
     analizy.get_data()
 
-    biznesr = finscrap.GetAssetBiznesR("biznesradar.pl", funds_urls)
-    biznesr.get_data()
+    #  biznesr = finscrap.GetAssetBiznesR("biznesradar.pl", funds_urls)
+    #  biznesr.get_data()
 
-    borsa = finscrap.GetAssetBorsa("borsa", funds_urls)
-    borsa.get_data()
+    #  borsa = finscrap.GetAssetBorsa("borsa", funds_urls)
+    #  borsa.get_data()
 
-    ishares = finscrap.GetAssetIShares("ishares", funds_urls)
-    ishares.get_data()
+    #  ishares = finscrap.GetAssetIShares("ishares", funds_urls)
+    #  ishares.get_data()
 
 
 if __name__ == "__main__":
